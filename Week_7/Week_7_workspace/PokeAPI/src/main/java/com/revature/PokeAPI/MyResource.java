6583fd703447aba77dff057b6bf6f9f9a9098d59
amelia <a.max.dunn@gmail.com>
2018-05-30 15:30:53 -0400

    added web services example PokeAPI

---
+++ Week_7/Week_7_workspace/PokeAPI/src/main/java/com/revature/PokeAPI/MyResource.java
@@ -0,0 +1,35 @@
+package com.revature.PokeAPI;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ * This resource is accessed from http://localhost:8080/PokeAPI/webapi/myresource
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    @GET
+    @Path("/{name}")
+    // This resource is accessed at http://localhost:8080/PokeAPI/webapi/myresource/**WhateverIsHere
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt(@PathParam("name") String name) {
+    	return "Got it, " + name + "!";
+    }
+}
