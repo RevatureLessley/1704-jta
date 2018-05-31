3da442b821e8811ae269eefa104da1a61dd8dee7
William Gentry <william.gentry02@gmail.com>
2018-05-31 17:55:37 -0400

    PokeAPI example as of this afternoon

---
+++ Web Services/PokeAPI/src/main/java/com/revature/service/PokemonService.java
@@ -0,0 +1,25 @@
+package com.revature.service;
+
+import java.util.List;
+
+import com.revature.dao.PokemonDao;
+import com.revature.dao.PokemonDaoImpl;
+import com.revature.model.Pokemon;
+
+public class PokemonService {
+
+	private static PokemonDao dao = PokemonDaoImpl.getInstance();
+	private PokemonService() {}
+	
+	public static List<Pokemon> getAllPokemon() {
+		return dao.getAllPokemon();
+	}
+	
+	public static Pokemon getPokemon(String name) {
+		return dao.getPokemon(name);
+	}
+	
+	public static boolean insertPokemon(Pokemon pokemon) {
+		return dao.insertPokemon(pokemon);
+	}
+}
